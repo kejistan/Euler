0bf553c45a56392460295cfcad73d3d07c8db8c9
Sean Nicolay <kejistan@gmail.com>
2010-02-07 03:24:52 -0600

    Created euler helper module

---
+++ euler.hs
@@ -0,0 +1,34 @@
+module Euler
+       (factors
+       ,fermat) where
+
+import qualified Data.List as List
+import qualified Data.Map as Map
+
+isqrt :: Integer -> Integer
+isqrt = ceiling . sqrt . fromIntegral
+
+fermat :: Integer -> [Integer]
+fermat n = fermat' n [isqrt n..n]
+
+fermat' :: Integer -> [Integer] -> [Integer]
+fermat' _ [] = []
+fermat' n (x:xs)
+  | y^2 /= x^2 - n = fermat' n xs
+  | x - y == 1     = [ x + y ]
+  | otherwise      = concatMap fermat [ x - y, x + y ]
+    where y = isqrt $ x^2 - n
+          
+factors :: (Integer -> [Integer]) -> Integer -> [Integer]
+factors f n
+  | even n = 2 : factors f (n `div` 2)
+  | otherwise = f n
+
+lcm :: [Integer] -> Integer
+lcm x
+  | null x    = 0
+  | otherwise = Map.foldWithKey (\k d bcc -> bcc * k ^ d) 1 primeCounts
+    where
+      allFactors = [ factors (fermat) y | y <- x ]
+      groups = concat [ List.group (List.sort b) | b <- allFactors ]
+      primeCounts = List.foldl (\acc c -> Map.insertWith (max) (head c) (List.genericLength c) acc) Map.empty groups
